22b80289232106acb6b9844f2e75c6550a39085c
Paul Fenwick <pjf@perltraining.com.au>
2013-03-13 12:02:30 +1100

    Works as standalone.

--- lib/App/StalkSquare.pm
+++ lib/App/StalkSquare.pm
@@ -3,14 +3,57 @@ use v5.010;
 use strict;
 use warnings;
 use autodie;
-use Moose;
-use MooseX::Method::Signatures;
+use File::Slurp qw(read_file);
+use WWW::Mechanize;
+use JSON::Any;
+use Getopt::Std;
 
+my %opts = (
+    p   => undef,       # Phone
+    e   => undef,       # email
+    t   => undef,       # twitter
+    f   => undef,       # Facebook
+    n   => undef,       # Name
+    T   => undef,       # Twitter source
+);
 
+my %search_type = (
+    phone         => 'p',
+    email         => 'e',
+    twitter       => 't',
+    twitterSource => 'T',
+    fbid          => 'f',
+    name          => 'n',
+);
 
+my $HOME = $ENV{HOME} || $ENV{UserProfile};
 
-=for Pod::Coverage BUILD
+my $API_BASE = 'https://api.foursquare.com/v2';
+my $token    = read_file("$HOME/.4sq_token");
+chomp($token);
+$token    = "oauth_token=$token&v=20121108";
 
-=cut
+my $SEARCH_BASE = $API_BASE . '/users/search?';
+
+my $mech = WWW::Mechanize->new;
+my $json = JSON::Any->new;
+
+getopts('p:e:t:f:n:T:', \%opts);
+
+while (my ($label,$short) = each %search_type) {
+    next unless $opts{$short};
+
+    # Otherwise, do call.
+
+    my $url = $SEARCH_BASE."$label=$opts{$short}&$token";
+
+    say "Fetching $url";
+
+    $mech->get($url);
+
+    use Data::Dumper;
+    say Dumper $json->decode( $mech->content );
+
+}
 
 1;
